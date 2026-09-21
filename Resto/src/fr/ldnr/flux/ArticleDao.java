02860ce3d9cc5fc5c8ce93dd7a550951eb8b3e51
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-21 14:10:01 +0200

    feat: Add constructor and init all methods required.

--- Resto/src/fr/ldnr/flux/ArticleDao.java
+++ Resto/src/fr/ldnr/flux/ArticleDao.java
@@ -3,9 +3,44 @@
  */
 package fr.ldnr.flux;
 
+import java.sql.Statement;
+import java.sql.Connection;
+
+import fr.ldnr.jdbc.Article;
+
 /**
  * 
  */
-public class ArticleDao {
+public class ArticleDao extends Dao<Article> {
+	/**
+	 * 
+	 */
+	public ArticleDao(Connection conn) {
+		super(conn);
+	}
+
+	@Override
+	public boolean create(Article obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Article read(int idj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean update(Article obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Article obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
