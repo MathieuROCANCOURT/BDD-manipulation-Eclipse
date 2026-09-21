54deae34bf2dbccf0cfa5914dbcf3c60cc285a72
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-21 15:55:38 +0200

    feat: Complete read method in ArticleDao class.

--- Resto/src/fr/ldnr/flux/ArticleDao.java
+++ Resto/src/fr/ldnr/flux/ArticleDao.java
@@ -3,8 +3,12 @@
  */
 package fr.ldnr.flux;
 
-import java.sql.Statement;
+import java.util.logging.*;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import fr.ldnr.jdbc.Article;
 
@@ -21,25 +25,41 @@ public class ArticleDao extends Dao<Article> {
 
 	@Override
 	public boolean create(Article obj) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public Article read(int idj) {
-		// TODO Auto-generated method stub
-		return null;
+	public Article read(int id) throws SQLException {
+		Article article = new Article();
+		Statement statement = null;
+		String execute = "SELECT Description, Brand, UnitaryPrice FROM articles WHERE idArticle = ?";
+
+		try (PreparedStatement ps = this.connect.prepareStatement(execute)) {
+			ps.setInt(1, id);
+			statement = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			statement.executeQuery(execute);
+			if (statement.getResultSet().first()) {
+				article = new Article(id, statement.getResultSet().getString("Description"),
+						statement.getResultSet().getString("Brand"), statement.getResultSet().getFloat("UnitaryPrice"));
+			}
+		} catch (SQLException e) {
+			Logger logger = Logger.getAnonymousLogger();
+			logger.warning(e.getLocalizedMessage());
+		} finally {
+			if (statement != null) {
+				statement.close();
+			}
+		}
+		return article;
 	}
 
 	@Override
 	public boolean update(Article obj) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean delete(Article obj) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
