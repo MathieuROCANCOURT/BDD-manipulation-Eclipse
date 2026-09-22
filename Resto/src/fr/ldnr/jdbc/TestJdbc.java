c67f4ead362420ea405712fdca7c29a1f9ddcc1a
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-22 13:24:27 +0200

    test: Test readAll method and display all articles.

--- Resto/src/fr/ldnr/jdbc/TestJdbc.java
+++ Resto/src/fr/ldnr/jdbc/TestJdbc.java
@@ -5,6 +5,7 @@ package fr.ldnr.jdbc;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.logging.Logger;
 
 import fr.ldnr.flux.ArticleDao;
@@ -21,6 +22,13 @@ public class TestJdbc {
 		ArticleDao articleDao = new ArticleDao();
 
 		try (Connection conenction = articleDao.getConnection()) {
+			List<Article> listArticles = articleDao.readAll();
+			System.out.println("--------- Voici la liste de toutes les articles --------------");
+			for (int index = 0; index < listArticles.size(); index++) {
+				System.out.println(listArticles.get(index));
+			}
+			System.out.println("-------------------------------------------------------------");
+			
 			Article article = articleDao.read(2);
 			System.out.println(article);
 		} catch (SQLException e) {
