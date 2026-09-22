40691ccd8867b6d7ac1939049040548423de7a44
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-22 13:36:26 +0200

    Add all getteurs and setteurs in Article class.

--- Resto/src/fr/ldnr/jdbc/Article.java
+++ Resto/src/fr/ldnr/jdbc/Article.java
@@ -22,6 +22,38 @@ public class Article {
 		this.price = price;
 	}
 	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
 	@Override
 	public String toString() {
 		return "Article " + this.id + ": " + this.description + ", marque: " + this.brand + ", prix: " + this.price + '.';
