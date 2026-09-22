26c031a8b47a25e45fe9bf7371b707ff15432028
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-22 11:42:38 +0200

    fix: Resolve error request SQL and affect the value in SQL request in read method in ArticleDao class.

--- Resto/src/fr/ldnr/flux/ArticleDao.java
+++ Resto/src/fr/ldnr/flux/ArticleDao.java
@@ -3,6 +3,7 @@
  */
 package fr.ldnr.flux;
 
+import java.util.Optional;
 import java.util.logging.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -32,34 +33,30 @@ public class ArticleDao implements Dao<Article> {
 
 		return DriverManager.getConnection(URL, LOGIN, PASSWORD);
 	}
+
 	/**
 	 * 
 	 */
-	public ArticleDao(Connection conn) {
 	public void create(Article obj) {
 	}
 
 	@Override
-	public Article read(int id) throws SQLException {
-		Article article = new Article();
-		Statement statement = null;
-		String execute = "SELECT Description, Brand, UnitaryPrice FROM articles WHERE idArticle = ?";
+	public Optional<Article> read(int id) throws SQLException {
+		Optional<Article> article = Optional.empty();
+		String execute = "SELECT Description, Brand, UnitaryPrice FROM t_articles WHERE idArticle = ?;";
 
-		try (PreparedStatement ps = this.connect.prepareStatement(execute)) {
+		try (PreparedStatement ps = this.getConnection().prepareStatement(execute)) {
 			ps.setInt(1, id);
-			statement = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-			statement.executeQuery(execute);
-			if (statement.getResultSet().first()) {
-				article = new Article(id, statement.getResultSet().getString("Description"),
-						statement.getResultSet().getString("Brand"), statement.getResultSet().getFloat("UnitaryPrice"));
+
+			try (ResultSet rs = ps.executeQuery()) {
+				if (rs.next()) {
+					article = Optional.of(new Article(id, rs.getString("Description"), rs.getString("Brand"),
+							rs.getFloat("UnitaryPrice")));
+				}
 			}
 		} catch (SQLException e) {
 			Logger logger = Logger.getAnonymousLogger();
 			logger.warning(e.getLocalizedMessage());
-		} finally {
-			if (statement != null) {
-				statement.close();
-			}
 		}
 		return article;
 	}
