d869ad9de187c1b207a8ddbc4d506d0754d23283
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-22 14:29:18 +0200

    chore: Add throws to all methods in Dao class.

--- Resto/src/fr/ldnr/flux/Dao.java
+++ Resto/src/fr/ldnr/flux/Dao.java
@@ -10,13 +10,13 @@ import java.util.List;
  * 
  */
 public interface Dao<T> {
-	public void create(T t);
+	public void create(T t) throws SQLException;
 	
 	public List<T> readAll() throws SQLException;
 
 	public T read(int id) throws SQLException;
 
-	public void update(T t);
+	public void update(T t) throws SQLException;
 
-	public void delete(T t);
+	public void delete(T t) throws SQLException;
 }
