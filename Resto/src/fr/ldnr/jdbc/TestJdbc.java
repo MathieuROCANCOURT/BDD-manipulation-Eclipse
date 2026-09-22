83a7cfd74b41f6540985845ab6f84c3dc79cc321
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-22 15:18:22 +0200

    fix: Add SQL request to reset AUTO_INCREMENT to 1 in t_articles table.

--- Resto/src/fr/ldnr/jdbc/TestJdbc.java
+++ Resto/src/fr/ldnr/jdbc/TestJdbc.java
@@ -4,6 +4,7 @@
 package fr.ldnr.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.logging.Logger;
@@ -21,22 +22,26 @@ public class TestJdbc {
 	public static void main(String[] args) throws Exception {
 		ArticleDao articleDao = new ArticleDao();
 
-		try (Connection conenction = articleDao.getConnection()) {
+		try (Connection conenction = articleDao.getConnection();
+				PreparedStatement st = articleDao.getConnection()
+						.prepareStatement("ALTER TABLE t_articles AUTO_INCREMENT = 1");) {
+			st.executeUpdate();
+
 			Article article = new Article("Disquette", "IBM", 1.43f);
 			articleDao.create(article);
-			
+
 			List<Article> listArticles = articleDao.readAll();
 			System.out.println("--------- Voici la liste de toutes les articles --------------");
 			for (int index = 0; index < listArticles.size(); index++) {
 				System.out.println(listArticles.get(index));
 			}
 			System.out.println("-------------------------------------------------------------");
-			
+
 			article.setDescription("CD-ROM 32 bits");
 			articleDao.update(article);
-			
+
 			System.out.println(articleDao.read(2));
-			
+
 			for (int index = 12; index < listArticles.size(); index++) {
 				articleDao.delete(listArticles.get(index));
 			}
